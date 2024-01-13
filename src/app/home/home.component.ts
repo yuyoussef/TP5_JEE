3f969da05f1eaea2ae8e064cbd5dc8db3d0ba4d6
¨yuyoussef¨ <youssef.taoussi6@gmail.com>
2024-01-13 12:20:24 +0100

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
